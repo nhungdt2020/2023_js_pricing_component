807913d8dc8ef604d120cbb7baf9ad1b4f7602c1
nhungdt2020 <64959340+nhungdt2020@users.noreply.github.com>
2023-08-01 17:08:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// get status default
+
+ 
+
+
+//check status when click toggle button
+
+// $("#switch").click(function () {
+
+//   let toggleClickStatus = $(":checkbox").is(":checked");
+//   console.log('toggleClickStatus',toggleClickStatus);
+
+//   if (toggleClickStatus === true) {
+//     $(".pricing-pro").text(`$24.99`);
+//     $(".pricing-master").text(`$39.99`);
+//     $(".pricing-basic").text(`$19.99`);
+
+//   } else {
+//     $(".pricing-pro").text(`$249.99`);
+//     $(".pricing-master").text(`$399.99`);
+//     $(".pricing-basic").text(`$199.99`);
+//   }
+// });
+
+
